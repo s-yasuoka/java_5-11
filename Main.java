524d9474f7928a506e318af3eb1b3d419c3ff416
s-yasuoka <migayasu@gmail.com>
2022-08-10 15:32:28 +0900

    fin

--- Main.java
+++ Main.java
@@ -7,5 +7,6 @@ public class Main{
   }
   public static void main(String[] args){
     System.out.println("10 + 20 = " + add(10,20));
+    System.out.println("10 + 20 + 30 = " + add(10,20,30));
   }
 }
